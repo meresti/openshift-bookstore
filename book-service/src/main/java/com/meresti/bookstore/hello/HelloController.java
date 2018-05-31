9d0f30e0abad715f40ecc5033ee2abc40a8d41dd
Zsolt Horvat <zsolt.horvat@accenture.com>
2018-05-31 16:16:34 +0300

    repackage

--- book-service/src/main/java/com/meresti/bookstore/hello/HelloController.java
+++ book-service/src/main/java/com/meresti/bookstore/hello/HelloController.java
@@ -1,4 +1,4 @@
-package com.meresti.bookstore.bookservice;
+package com.meresti.bookstore.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
